9e810e9781ba394c28e946a6301de453740365c6
russell258 <duckie258@gmail.com>
2023-06-20 12:26:07 +0800

    set up dependencies

---
+++ src/main/java/sg/edu/nus/iss/paf_day1/PafDay1Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay1Application.class, args);
+	}
+
+}
