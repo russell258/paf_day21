11b6652e6207d5d563ad6447444c4ac852d9e386
russell258 <duckie258@gmail.com>
2023-06-21 13:49:55 +0800

    more error exception handler

--- src/main/java/sg/edu/nus/iss/paf_day1/exception/GlobalExceptionHandler.java
+++ src/main/java/sg/edu/nus/iss/paf_day1/exception/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package sg.edu.nus.iss.paf_day1.exception;
 
+import java.io.IOException;
 import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,4 +34,23 @@ public class GlobalExceptionHandler {
         LOGGER.error(ex.getMessage());
         return new ResponseEntity<ErrorMessage>(errMsg, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(IOException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorMessage> handleIOException(HttpServletRequest req, Exception ex){
+        ErrorMessage errMsg = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), new Date(), ex.getMessage(), req.getRequestURI());
+    
+        LOGGER.info("Enter handleIOxception");
+        return ResponseEntity.internalServerError().body(errMsg);
+    }
+
+    @ExceptionHandler(CannotGetJdbcConnectionException.class)
+    public ResponseEntity<ErrorMessage> handleException(Exception ex, HttpServletRequest request){
+        ErrorMessage errMsg = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(),new Date(), ex.getMessage(),request.getRequestURL().toString());
+
+        LOGGER.error(ex.getMessage());
+
+        return new ResponseEntity<ErrorMessage>(errMsg, HttpStatus.NOT_FOUND);
+    }
+
 }
